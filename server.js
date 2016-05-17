2aade07c9922336f55da0c61e06715aa7f310c12
covega <john.morgan@live.com>
2016-05-17 11:09:35 -0700

    added basic example of passing html from scraped page to index

--- server.js
+++ server.js
@@ -11,17 +11,17 @@ app.get('/', function(req, res){
 app.get('/scrape', function(req, res){
 	var url = 'http://www.southandeastbayairportshuttle.com/';
 
-	request(url, function(error, response, html){
+	request(url, function(error, response, html){ 
 		if(error){
 			console.log('we have a problem: ' + error);
-			throw error;
+			response.status(500).send(JSON.stringify(error));
+			return;
 		}
-		console.log('time to parse');
 		var $ = cheerio.load(html); //this lets cheerio act just like jquery
 
-		var title = $('title');
+		var title = $('title').html();
 		
-
+		res.send(JSON.stringify(title));
 	});
 });
 
