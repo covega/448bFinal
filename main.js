82fdfa7238acfd453eeaa34449045521f5b598c6
covega <john.morgan@live.com>
2016-05-31 23:38:18 -0700

    redesign now opens in an iframe
    needs styling

--- main.js
+++ main.js
@@ -16,7 +16,8 @@ $('#redesign').click(function() {
 
  	xhr.onreadystatechange = function(){
  		if(xhr.readyState == 4 && xhr.status == 200){
- 			console.log(xhr.responseText);		
+ 			console.log(xhr.responseText);
+ 			$("#redesignIframe").cntents().find('html').html(xhr.responseText);
  		}
  	} 	
 
